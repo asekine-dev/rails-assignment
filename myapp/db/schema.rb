22f3a525723e76a05d7629a236f526d161159a8c
asekine-dev <eight.month.silicofluoride1016pk@gmail.com>
2026-01-27 21:00:50 +0900

    chore: email 重複防止のため unique index を追加

--- myapp/db/schema.rb
+++ myapp/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_01_27_111308) do
+ActiveRecord::Schema[8.1].define(version: 2026_01_27_114108) do
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
@@ -52,6 +52,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_01_27_111308) do
     t.string "email"
     t.string "password_digest"
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
